d2dc2d0ca8fee7e1c17c5d9172811799e34c4f0e
OusmaneMballo <ousmanemballo576@gmail.com>
2020-05-16 18:26:17 +0000

    Calculer la puissance avec une boucle

--- Exercice4.py
+++ Exercice4.py
@@ -4,9 +4,23 @@ Ecrire un programme qui saisit un réel x et un entier n et affiche x à la puis
 Version 1 : utiliser la fonction pow  du fichier d’en-tête <math.h>  ex : pow(x,n)
 """
 #Resolution V1:
-
+"""
 print("Entrer un entier x")
 x=int(input())
 print("Enter sa puissance n")
 n=int(input())
 print(x,"^",n,"=",pow(x,n))
+"""
+#Resolution V1:
+"""
+Calcul la puissance en utilisant une boucle
+"""
+
+print("Entrer un entier x")
+x = int(input())
+print("Enter sa puissance n")
+n = int(input())
+val=x
+for i in range(1,n):
+    val=val*x
+print(x, "^", n, "=",val)
