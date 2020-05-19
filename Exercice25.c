0cf2195b15088affffcd42bf32a5a6790a54e52d
OusmaneMballo <ousmanemballo576@gmail.com>
2020-05-19 10:52:26 +0000

    Ecrire le code C du programme qui affiche le texte suivant pour les chiffres de 1 à 10 : 1 2 2 3 3 3

---
+++ Exercice25.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*Ecrire le code C du programme qui affiche le texte suivant pour les chiffres de 1 à 10 :
+1 
+2 2 
+3 3 3
+ */
+int main(){
+int i,i;
+for(i=1;i<=10;i++){
+for(j=1;j<=i;j++){
+printf(“%d”,i);
+}
+printf(“\n”);
+}
+return 0;
+}
